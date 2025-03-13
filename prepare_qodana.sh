27d0ae95b88c88c97780b22cf3b3fbcac9697274
Andrew Hariyanto <51850304+andrewhariyanto@users.noreply.github.com>
2025-03-14 01:18:50 +0700

    test2

--- prepare_qodana.sh
+++ prepare_qodana.sh
@@ -23,9 +23,12 @@ fi
 # Run Unity in headless mode to sync solution for Rider
 echo "Running Unity to sync Rider solution..."
 
-UNITY_EXECUTABLE="$UNITY_EDITOR_PATH"
+echo $UNITY_EDITOR_PATH
+echo $UNITY_PROJECT_PATH
+
+UNITY_EXECUTABLE=$UNITY_EDITOR_PATH
 
 ${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' unity-editor} \
-  -batchmode -quit -projectPath "$UNITY_PROJECT_PATH" -executeMethod Packages.Rider.Editor.RiderScriptEditor.SyncSolution
+  -batchmode -quit -projectPath $UNITY_PROJECT_PATH -executeMethod Packages.Rider.Editor.RiderScriptEditor.SyncSolution
 
 echo "Unity Rider solution sync completed."
